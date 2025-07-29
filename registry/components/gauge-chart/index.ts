a44c5ee013bde12308c05bb88c896b05407d97cd
chiaoyyc <chiaoyyc@gmail.com>
2025-07-30 01:14:19 +0800

    refactor: remove old component structure
    - Remove legacy component directories for reorganization
    - Clean up old chart implementations and unused files
    - Prepare codebase for new architectural structure

--- registry/components/gauge-chart/index.ts
+++
@@ -1,8 +0,0 @@
-export { GaugeChart } from './gauge-chart'
-export type { 
-  GaugeChartProps, 
-  GaugeChartDataPoint, 
-  ProcessedGaugeDataPoint,
-  GaugeZone 
-} from './types'
-export { default as gaugeChartStyles } from './gauge-chart.css?inline'
--- registry/components/scatter-plot/index.ts
+++
@@ -1,2 +0,0 @@
-export { ScatterPlot } from './scatter-plot'
-export type { ScatterPlotProps, ProcessedDataPoint } from './types'
