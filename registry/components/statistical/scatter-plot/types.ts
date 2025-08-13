f238584e18e952c47b42da13b799edaa4d9ae4aa
chiaoyyc <chiaoyyc@gmail.com>
2025-08-14 06:52:01 +0800

    feat: Refactor ScatterPlot to extend BaseChart

--- registry/components/statistical/scatter-plot/types.ts
+++ registry/components/statistical/scatter-plot/types.ts
@@ -1,47 +1,13 @@
-import { BaseChartProps } from '../../types'
+import { HTMLAttributes, ReactNode } from 'react';
+import { ScatterPlotProps as CoreScatterPlotProps, ProcessedScatterDataPoint as CoreProcessedScatterDataPoint } from './core/types';
 
-export interface ProcessedDataPoint {
-  x: number
-  y: number
-  size?: number
-  color?: string
-  originalData: any
+export type { Margin, DataMapping, ProcessedScatterDataPoint } from './core/types';
+
+export interface ScatterPlotProps extends CoreScatterPlotProps, Omit<HTMLAttributes<HTMLDivElement>, 'onHover'> {
+  className?: string;
+  style?: React.CSSProperties;
+  tooltipFormat?: (d: CoreProcessedScatterDataPoint) => ReactNode;
 }
 
-export interface ScatterPlotProps extends BaseChartProps {
-  // 點樣式
-  radius?: number
-  minRadius?: number
-  maxRadius?: number
-  
-  // 大小映射
-  sizeKey?: string
-  sizeAccessor?: (d: any) => number
-  sizeRange?: [number, number]
-  
-  // 顏色映射
-  colorKey?: string
-  colorAccessor?: (d: any) => string
-  colorScale?: string[]
-  
-  // 趨勢線
-  showTrendline?: boolean
-  trendlineColor?: string
-  trendlineWidth?: number
-  
-  // 分群顏色
-  groupKey?: string
-  
-  // 點的樣式
-  opacity?: number
-  strokeWidth?: number
-  strokeColor?: string
-  
-  // 工具提示
-  showTooltip?: boolean
-  tooltipFormat?: (d: ProcessedDataPoint) => string
-  
-  // 縮放和拖拽
-  enableZoom?: boolean
-  enableBrush?: boolean
-}
+// 保持向下兼容
+export type ProcessedDataPoint = CoreProcessedScatterDataPoint;
