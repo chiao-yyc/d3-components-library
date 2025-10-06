df8ecbedb9ba5918f66e877da978db6b24b616be
chiaoyyc <chiaoyyc@gmail.com>
2025-10-06 12:42:14 +0800

    feat: add Vercel configuration file for deployment setup

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
